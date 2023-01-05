d5241db2de1b8e44c542f7e98c2d0e2ec0cadb28
Svitlana Yudina <osbka31@gmail.com>
2023-01-05 17:22:58 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Link
-          className="App-link"
-          to="/"
-        >
+        <Link className="App-link" to="/">
           Home Page
         </Link>
-        <Link
-          className="App-link"
-          to="/phones"
-        >
+        <Link className="App-link" to="/phones">
           PhonesPage
         </Link>
       </header>
       <main>
         <Routes>
-        <Route
-            path="/"
-            element={<h1>Home Page</h1>}
-          />
-          <Route
-            path="phones"
-            element={<PhonesPage />}
-          />
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="phones" element={<PhonesPage />} />
         </Routes>
-
       </main>
     </div>
   );
