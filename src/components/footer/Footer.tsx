2576e438849238ac2f579f209a27a023d3179c90
Mikhail Kucherina <kucherinamihail@gmail.com>
2023-01-06 11:11:38 +0200

    fonts was added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,47 +1,35 @@
 import React from 'react';
 import './Footer.scss';
-
-export const Logo: React.FC = () => {
-  return (
-    <div className="logo" />
-  );
-};
+import logo from '../../images/Logo.png';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <Logo />
+      <img
+        src={logo}
+        alt="logo"
+        className="logo"
+      />
 
       <div className="footer__nav">
-        <a
-          href="/github"
-          className="footer__nav__link"
-        >
+        <a href="/github" className="footer__nav__link">
           github
         </a>
 
-        <a
-          href="/contacts"
-          className="footer__nav__link"
-        >
+        <a href="/contacts" className="footer__nav__link">
           contacts
         </a>
 
-        <a
-          href="/rights"
-          className="footer__nav__link"
-        >
+        <a href="/rights" className="footer__nav__link">
           rights
         </a>
       </div>
 
       <div className="footer__topscroll">
         Back to top
-
         <button
           type="button"
           className="footer__topscroll__button"
-          aria-label="Save"
           onClick={() => window.scrollTo({ top: 0 })}
         />
       </div>
