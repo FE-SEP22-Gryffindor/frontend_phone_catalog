c2b85312ec0f1ddff9d036eee095783e37f1da19
Mikhail Kucherina <kucherinamihail@gmail.com>
2023-01-06 11:22:58 +0200

    href to github was added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,22 +5,21 @@ import logo from '../../images/Logo.png';
 export const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <img
-        src={logo}
-        alt="logo"
-        className="logo"
-      />
+      <img src={logo} alt="logo" className="logo" />
 
       <div className="footer__nav">
-        <a href="/github" className="footer__nav__link">
+        <a
+          href="https://github.com/FE-SEP22-Gryffindor"
+          className="footer__nav__link"
+        >
           github
         </a>
 
-        <a href="/contacts" className="footer__nav__link">
+        <a href="/" className="footer__nav__link">
           contacts
         </a>
 
-        <a href="/rights" className="footer__nav__link">
+        <a href="/" className="footer__nav__link">
           rights
         </a>
       </div>
