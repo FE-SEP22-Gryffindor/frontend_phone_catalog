a0d6c392cb30b2eff4c734b2d252ee80b293262f
Svitlana Yudina <osbka31@gmail.com>
2023-01-07 10:18:09 +0200

    linter fixes in other files

--- src/modules/PhonesPage/PhonesPage.tsx
+++ src/modules/PhonesPage/PhonesPage.tsx
@@ -23,10 +23,10 @@ const phones = [
 
 export const PhonesPage = () => {
   return (
-    <div className='container-phone-page'>
-      <div className='breadcrumbs'>
-        <a className='icon icon--home'></a>
-        <a className=''>Phones</a>
+    <div className="container-phone-page">
+      <div className="breadcrumbs">
+        <a className="icon icon--home"></a>
+        <a className="">Phones</a>
       </div>
       <div>
         <h1 className="title">Mobile phones</h1>
@@ -34,32 +34,29 @@ export const PhonesPage = () => {
       <div>
         <p className="result-items">95 models</p>
       </div>
-      <div className='sort-items'>
-      <div>
-      <label htmlFor="phones-sort">Sort buy:</label>
-        <select name="phones-sort" id="phones-sort">
-          <option value="newest">Newest</option>
-        </select>
+      <div className="sort-items">
+        <div>
+          <label htmlFor="phones-sort">Sort buy:</label>
+          <select name="phones-sort" id="phones-sort">
+            <option value="newest">Newest</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="page-items">Items on page:</label>
+          <select name="page-items" id="page-items">
+            <option value="16">16</option>
+            <option value="32">32</option>
+          </select>
+        </div>
       </div>
-      <div>
-
-      <label htmlFor="page-items">Items on page:</label>
-        <select name="page-items" id="page-items">
-          <option value="16">16</option>
-          <option value="32">32</option>
-        </select>
-      </div>
-      </div>
-      <div className='products-catalog'>
-        {phones.map(phone => (
-          <div className='products-catalog__card' key={phone.id}>
+      <div className="products-catalog">
+        {phones.map((phone) => (
+          <div className="products-catalog__card" key={phone.id}>
             {`${phone.title} ${phone.id}`}
           </div>
         ))}
       </div>
-      <div>
-        pagination
-      </div>
+      <div>pagination</div>
     </div>
   );
 };
