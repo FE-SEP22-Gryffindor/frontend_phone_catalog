6db464e5561a6f1b0a29b3f76efc2ca7152879b0
Svitlana Yudina <osbka31@gmail.com>
2023-01-09 16:15:04 +0200

    link BACK realy goes back

--- src/modules/CartPage/CartPage.tsx
+++ src/modules/CartPage/CartPage.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-shadow */
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './CartPage.scss';
 
 // type Item = {
@@ -11,13 +12,22 @@ import './CartPage.scss';
 
 const items = [
   {
-    id: 1, img: 'link', title: 'title', price: 1999,
+    id: 1,
+    img: 'link',
+    title: 'title',
+    price: 1999,
   },
   {
-    id: 2, img: 'link', title: 'title', price: 1999,
+    id: 2,
+    img: 'link',
+    title: 'title',
+    price: 1999,
   },
   {
-    id: 3, img: 'link', title: 'title', price: 1999,
+    id: 3,
+    img: 'link',
+    title: 'title',
+    price: 1999,
   },
 ];
 
@@ -25,32 +35,44 @@ const sum = items.reduce(
   (accumulator, currentValue) => accumulator + currentValue.price,
   0,
 );
+// const back = history.back();
+
+// console.log(back);
 
 const total = items.length;
 
 export const CartPage = () => {
   return (
-    <div className='container-card-page'>
+    <div className="container-card-page">
       <div className="breadcrumbs">
-        <Link to='/' className="back">Back</Link>
+        {/* <Link to='/' className="back">Back</Link> */}
+        <a
+          className="back"
+          onClick={(event) => {
+            event.preventDefault();
+            history.back();
+          }}
+        >
+          Back
+        </a>
       </div>
       <div>
         <h1 className="title">Cart page</h1>
       </div>
-      <div className='cart-form'>
-        <div className='cart-items'>
-          {items.map(item => (
-            <div className='cart-item' key={item.id}>
+      <div className="cart-form">
+        <div className="cart-items">
+          {items.map((item) => (
+            <div className="cart-item" key={item.id}>
               <h2>{item.title}</h2>
             </div>
           ))}
         </div>
 
-        <div className='cart-total'>
-          <h2 className='total-amount'>${sum}</h2>
-          <p className='total-items'>Total for {total} items</p>
-          <hr className='total-hr'/>
-          <button className='btn-checkout'>Checkout</button>
+        <div className="cart-total">
+          <h2 className="total-amount">${sum}</h2>
+          <p className="total-items">Total for {total} items</p>
+          <hr className="total-hr" />
+          <button className="btn-checkout">Checkout</button>
         </div>
       </div>
     </div>
