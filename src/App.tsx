30cf39f6833105407578b0f0cbbf5d97c47c17e0
Svitlana Yudina <osbka31@gmail.com>
2023-01-11 11:27:53 +0200

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
-// import { PhoneCard } from './components/PhoneCard';
 import { PhonesPage } from './modules/PhonesPage';
-import { Footer } from './components/footer';
-import { NotFoundPage } from './modules/NotFoundPage';
-import { CartPage } from './modules/CartPage';
-import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { NotFoundPage } from './modules/NotFoundPage';
 import { CartPage } from './modules/CartPage';
