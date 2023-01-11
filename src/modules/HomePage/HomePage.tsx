e5b1a03fbfb40ad15064fb742cf06d99e79f1577
Taras Pustovyi <taraselnik@gmail.com>
2023-01-11 14:16:29 +0200

    Init home page

---
+++ src/modules/HomePage/HomePage.tsx
@@ -0,0 +1,39 @@
+
+import React from 'react';
+// import { Carousel } from '../../components/Carousel';
+// import { ProductsSlider } from '../../components/ProductsSlider';
+// import { CategoryNav } from '../../components/CategoryNav';
+import './HomePage.scss';
+
+export const HomePage = () => {
+  return (
+    <div className="home">
+      <div className="container container--with-min-height">
+        <section className="home__section">
+          {/* <Carousel /> */}
+          COROUSEL
+        </section>
+        <section className="home__section">
+          {/* <ProductsSlider
+            title="Hot prices"
+            filterCriteria="discount"
+            sortBy="discount-value"
+          /> */}
+          ProductsSlider - Brand New Models
+        </section>
+        <section className="home__section">
+          {/* <CategoryNav /> */}
+          CategoryNav - SHOP by category
+        </section>
+        <section className="home__section">
+          {/* <ProductsSlider
+            title="Brand new models"
+            filterCriteria="no-discount"
+            sortBy="age"
+          /> */}
+          ProductsSlider - HOT prices
+        </section>
+      </div>
+    </div>
+  );
+};
